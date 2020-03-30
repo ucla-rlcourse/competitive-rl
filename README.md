3939196c51ee618ab9fb9bf0ad8dc0338eebe3b8
pengzh <pengzh@ie.cuhk.edu.hk>
2020-03-30 20:32:20 +0800

    add visualization script

--- README.md
+++ README.md
@@ -36,5 +36,28 @@ Action 0 represents "moving the left bat up", 1 for "no operation" and 2 for "mo
 
 In `CompetitivePongDouble-v0` environment, both agents receive observations shows that they are controlling the left bat. Namely the observation for second agent (the right-side agent) is mirrored. This design guarantees the agent trained by playing with rule-based agent controlling the left bat can smoothly be evaluated with other agents, even when it controls the right-side bat in reality.
 
+## Visualize builtin agents
+
+We provide a set of trained agents to serve as opponents. We prepared 6 level builtin agents, including:
+
+1. `RANDOM` - A random agent
+2. `RULE_BASED` - Rule_based agent that serves as the right agent in `CompetitivePong-v0`
+3. `WEAK` - Weak agent with a small neural network than yours
+4. `MEDIUM` - Medium agent with a small neural network
+5. `STRONG` - Strong agent with a large neural network as yours
+6. `ALPHA_PONG` - Boss-level agent that is well trained and may hard to beat down, use a large neural network as yours
+
+You can visualize the performance of arbitrary builtin agents and let them play against each other. Run the script:
+
+```bash
+# RULE_BASED agent VS RULE_BASED agent
+python vis.py
+
+# ALPHA_PONG agent vs STRONG agent
+python vis.py --left ALPHA_PONG --right STRONG --num-episodes 3
+```
+
+
+
 -----
 2019-2020 2nd term, IERG 6130: Reinforcement Learning and Beyond. Department of Information Engineering, The Chinese University of Hong Kong. Course Instructor: Professor ZHOU Bolei. Assignment author: PENG Zhenghao.
---
+++ vis.py
@@ -0,0 +1,42 @@
+import argparse
+import shutil
+
+from competitive_pong import get_builtin_agent_names, make_envs, \
+    get_compute_action_function, evaluate_two_policies
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--left", default="RULE_BASED", type=str,
+                        help="Left agent names, must in {}.".format(
+                            get_builtin_agent_names()))
+    parser.add_argument("--right", default="RULE_BASED", type=str,
+                        help="Right agent names, must in {}.".format(
+                            get_builtin_agent_names()))
+    parser.add_argument("--num-episodes", "-N", default=3, type=int,
+                        help="Number of episodes to run.")
+    args = parser.parse_args()
+
+    # collect builtin agents
+    agent_names = get_builtin_agent_names() + ["MY_AGENT"]
+    print("Agent names: ", agent_names)
+    print("Your chosen agents: left - {}, right - {}".format(
+        args.left, args.right))
+    assert args.left in agent_names, agent_names
+    assert args.right in agent_names, agent_names
+
+    # create env and setup policies
+    env = make_envs("CompetitivePongDouble-v0", num_envs=1, asynchronous=False,
+                    log_dir="tmp_vis").envs[0]
+    left = get_compute_action_function(args.left)
+    right = get_compute_action_function(args.right)
+
+    # evaluate
+    result = evaluate_two_policies(
+        left, right, env=env, render=True,
+        num_episode=args.num_episodes, render_interval=0.05  # 20 FPS rendering
+    )
+    print(result)
+
+    # clear
+    env.close()
+    shutil.rmtree("tmp_vis")
