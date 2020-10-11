80e2de7be7df74e3f2444a1741cb110fa0cee5b3
1155107756 <1483486410zy@gmail.com>
2020-10-11 19:48:19 +0800

    Create register.py

---
+++ competitive_rl/car_racing/register.py
@@ -0,0 +1,15 @@
+import gym
+from gym.envs.registration import register
+
+from competitive_rl.car_racing.car_racing_multi_players import CarRacing
+
+
+def register_competitive_envs():
+    register(
+        id="cCarRacing-v0",
+        entry_point=CarRacing,
+        max_episode_steps=1000,
+        reward_threshold=9000
+    )
+    print(
+        "Register car_racing_multiple_players environments.")
