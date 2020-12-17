e1c91384668ba09ceed6a4a265a63b93e29747f7
X.B. Wang <sanebow@gmail.com>
2020-12-17 21:12:50 +0800

    also include car icons for setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "tabulate",
         "box2d-py~=2.3.5"  # Identical to gym[box2d]
     ],
-    package_data={"competitive_rl": ["car_racing/fonts/*", "pong/*.ttf"]}
+    package_data={"competitive_rl": ["car_racing/fonts/*", "pong/*.ttf", "car_racing/*.png"]}
 )
 
 print("You have successfully installed competitive_rl 0.1.0!")
